793367d22553c51d27aeb9ba57378e537f492805
Maxwell1995 <Maxwellmjohn@gmail.com>
2023-05-10 16:30:53 +0300

    first commit

---
+++ app/src/main/java/com/example/maxapp/PageConstraintlayout.kt
@@ -0,0 +1,11 @@
+package com.example.maxapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageConstraintlayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_constraintlayout)
+    }
+}
